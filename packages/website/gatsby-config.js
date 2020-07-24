7974f861f7f9791a1b856e8272c3b90881ace06b
ertrzyiks <mateusz.derks@gmail.com>
2020-07-24 20:59:42 +0200

    chore(release): push website to gh pages

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/graph-utils`,
   siteMetadata: {
     title: `@ertrzyiks/graph-utils`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
