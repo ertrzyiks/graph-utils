86d6a81d5c23334093b7a4702afe95b74501ad07
ertrzyiks <mateusz.derks@gmail.com>
2020-08-04 20:56:24 +0200

    chore(website): add edge examples

--- packages/graph-utils/src/addEdgeInPlace.ts
+++ packages/graph-utils/src/addEdgeInPlace.ts
@@ -2,10 +2,10 @@ import {ExtractEdgeData, NodeId} from './types'
 
 export function addEdgeInPlace<Graph>(
   graph: Graph,
-  edge: { from: NodeId, to: NodeId, data: ExtractEdgeData<Graph> }
+  edge: { from: NodeId, to: NodeId } & ExtractEdgeData<Graph>
 ) {
   const { from, to, ...data } = edge
   const node = graph[from]
-  node.edges[to] = { data }
+  node.edges[to] = { ...data }
   return graph
 }
