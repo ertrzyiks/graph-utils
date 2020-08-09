cc1e44256224a06c072c4e36795148989d087a40
Mateusz Derks <mateusz.derks@gmail.com>
2020-08-09 21:22:16 +0200

    fix: retrieveClosestPath return array for a single path (#36)

--- packages/website/src/examples.ts
+++ packages/website/src/examples.ts
@@ -2,12 +2,14 @@ import Example1 from './examples/01-add-node'
 import Example2 from './examples/02-add-node-with-data'
 import Example3 from './examples/03-add-edge'
 import Example4 from './examples/04-add-edge-with-data'
+import Example5 from './examples/05-find-the-closest-path'
 
 const examples = {
   '01-add-node': Example1,
   '02-add-node-with-data': Example2,
   '03-add-edge': Example3,
-  '04-add-edge-with-data': Example4
+  '04-add-edge-with-data': Example4,
+  '05-find-the-closest-path': Example5
 }
 
 export default examples
