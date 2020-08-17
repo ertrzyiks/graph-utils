711d593f7081965062be1dda83d318fa6cab257b
Mateusz Derks <mateusz.derks@gmail.com>
2020-08-17 10:07:26 +0200

    chore(website): add parameters to api page (#51)

--- packages/graph-utils/src/addEdgeInPlace/addEdgeInPlace.ts
+++ packages/graph-utils/src/addEdgeInPlace/addEdgeInPlace.ts
@@ -2,6 +2,11 @@ import {ExtractEdgeData, NodeId} from '../types'
 
 type EdgeDefinitionOf<Graph> = { from: NodeId, to: NodeId } & ExtractEdgeData<Graph>
 
+/**
+ *
+ * @param graph Object to be updated
+ * @param edge
+ */
 export function addEdgeInPlace<Graph>(
   graph: Graph,
   edge: EdgeDefinitionOf<Graph>
