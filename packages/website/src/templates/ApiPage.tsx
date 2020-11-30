4d93c4e71c99a73464ac30cf7799b72f9b9cb8e7
Mateusz Derks <mateusz.derks@gmail.com>
2020-11-30 11:47:37 +0100

    chore: add table of contents to api page (#140)

--- packages/website/src/templates/ApiPage.tsx
+++ packages/website/src/templates/ApiPage.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import SEO from "../components/seo"
-import { Box } from '@material-ui/core'
+import { Box, List, ListItem, ListItemText, ListSubheader, Typography } from '@material-ui/core'
 import Layout from "../components/Layout"
 import ApiFunction from '../components/ApiFunction'
 import ApiType from '../components/ApiType'
 import { LoadAllFunctionsQuery } from '../graphqlTypes'
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 360
+  }
+}));
+
 interface ExamplePageProps {
   data: LoadAllFunctionsQuery
 }
 
+
 export default function ApiPage({ data }: ExamplePageProps) {
   const functions = data.allFunction.nodes
+
+  const classes = useStyles()
 
   return <Layout>
     <SEO title='API' />
 
+    <Box mb={10}>
+      <Typography variant='h5'>Table of Contents</Typography>
+
+      <Box mb={5}>
+        <List dense component='div' className={classes.root}>
+          <ListSubheader component='div' disableGutters>Types</ListSubheader>
+          <ListItem button component="a" href='#graph'>
+            <ListItemText>Graph</ListItemText>
+          </ListItem>
+          <ListItem button component="a" href='#nodeid'>
+            <ListItemText>NodeId</ListItemText>
+          </ListItem>
+
+          <ListSubheader component='div' disableGutters>Functions</ListSubheader>
+          {functions.map((fn, index) => (
+            <ListItem button component='a' href={`#${fn.slug}`}>
+              <ListItemText>{fn.name}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Box>
     <Box mb={10}>
       <ApiType name='Graph' slug='graph' signature='Graph<NodeData = {}, EdgeData = {}>'>
         Graph type is a core of the library. It accepts two generic parameters: shape of the custom properties of the nodes
